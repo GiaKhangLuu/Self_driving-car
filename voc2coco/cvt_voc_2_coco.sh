54f1ffd9a5c6f238f046773691832a5f3e410da3
GiaKhangLuu <waoknight@gmail.com>
2024-01-16 14:17:44 +0700

    add voc2coco module

---
+++ voc2coco/cvt_voc_2_coco.sh
@@ -0,0 +1,6 @@
+python voc2coco.py \
+    --ann_dir /Users/giakhang/Downloads/training_voc/VOC2012/Annotations \
+    --ann_ids /Users/giakhang/Downloads/training_voc/VOC2012/ImageSets/Main/trainval.txt \
+    --labels /Users/giakhang/Downloads/training_voc/VOC2012/labels.txt \
+    --output /Users/giakhang/Downloads/training_coco/trainval.json \
+    --ext xml
